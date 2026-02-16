4ac5288038a5f6b03543399b4d05f99b7a38c6d9
rarganaraz-bit <rarganaraz@alumnes.aulabalear.org>
2026-02-16 19:26:13 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php include 'config.php'; ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Gestión de Negocio</title>
+</head>
+<body>
+    <h1>Menú Principal</h1>
+    <ul>
+        <li><a href="clientes.php">Gestión de Clientes</a></li>
+        <li><a href="productos.php">Gestión de Productos</a></li>
+        <li><a href="pedidos.php">Gestión de Pedidos</a></li>
+    </ul>
+</body>
+</html>
